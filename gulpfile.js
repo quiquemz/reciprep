481274122021f9e87c26e25fc49276d3a92936aa
quiquemz <semendez@ucsd.edu>
2018-06-08 21:01:29 -0700

    Hack to show logout button on mobile

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,7 @@ gulp.task('browser-sync', ['nodemon'], function() {
   browserSync({
 
     // informs browser-sync to proxy our expressjs app which would run at the following location
-    proxy: 'http://localhost:3000',
+    proxy: 'http://localhost:3001',
 
     // informs browser-sync to use the following port for the proxied app
     // notice that the default port is 3000, which would clash with our expressjs
